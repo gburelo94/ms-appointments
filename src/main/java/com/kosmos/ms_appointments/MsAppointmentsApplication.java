cfb586cf76bc82e45ab57bd42252e18ec940ade0
Gaby Burelo <gaby.burelo@outlook.com>
2025-05-09 14:32:14 -0600

    chore Inital commit

---
+++ src/main/java/com/kosmos/ms_appointments/MsAppointmentsApplication.java
@@ -0,0 +1,13 @@
+package com.kosmos.ms_appointments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsAppointmentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsAppointmentsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kosmos/ms_appointments/MsAppointmentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kosmos.ms_appointments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsAppointmentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
